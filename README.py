a578f568b4d1224dd7fb0be54c68c0fb91c1374c
Greta <96105703+thisIsGreta@users.noreply.github.com>
2022-04-20 20:40:24 +0800

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,5 @@
 # Day 61/62 WTForm Notebook
-0) WTForm
+# 0) WTForm
     from flask_wtf import FlaskForm
     from wtforms import StringField, SubmitField, SelectField
     from wtforms.validators import DataRequired, URL
@@ -9,11 +9,11 @@
         coffee = SelectField('Coffee', choices=['✘️', '☕️', '☕️☕️', '☕️☕️☕️', '☕️☕️☕️☕️','☕️☕️☕️☕️☕️'])
     form = CafeForm(request.form)
 
-1) Flask-Bootstrap inheritance
-   from flask_bootstrap import Bootstrap
-   regarding main.py: 
+# 1) Flask-Bootstrap inheritance
+      from flask_bootstrap import Bootstrap
+    #regarding main.py: 
       Bootstrap(app)
-   regarding index.html: 
+    #regarding index.html: 
       {% extends 'bootstrap/base.html' %}
       {% block title %}Title{% endblock %}
       {% block styles %}
@@ -22,11 +22,11 @@
               href="{{url_for('static', filename='css/styles.css')}}">
       {% endblock %}
 
-2) Add quick wtform
+# 2) Add quick wtform
     {% import "bootstrap/wtf.html" as wtf %}
     {{ wtf.quick_form(form) }}
   
-3) Append rows to .csv file
+# 3) Append rows to .csv file
     import csv
     with open('cafe-data.csv', 'a') as menu:
         menu.write(f"\n{form.cafe.data},")
